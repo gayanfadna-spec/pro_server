9ba64b89dc61cd8778f4c597ba17b98b3e37031b
gayant41 <gayant41@gmail.com>
2026-04-22 14:00:08 +0530

    fix: update catch-all route to use regex for improved path matching

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
 
     // Catch-all route to serve the React index.html for all requests (fixes refresh 404s)
-    app.get('*', (req, res) => {
+    app.get(/.*/, (req, res) => {
         res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'));
     });
 } else {
